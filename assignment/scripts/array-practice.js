48dc7efba2b0377b4296013bd7c29a615d4e7bd3
pthomas0309 <preston.thomas355@gmail.com>
2021-04-13 23:18:22 -0500

    added 'everything bagels' to beggining of favoriteFoods and logged the new array

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -67,7 +67,8 @@ animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+favoriteFoods.unshift( 'Everything Bagels' );
+console.log( 'Just had my first everything bagel, my new favorite food!', favoriteFoods );
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
